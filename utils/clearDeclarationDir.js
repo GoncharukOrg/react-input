05c0fd2a0332437b355599e378b78f7b4fadace4
Nikolay Goncharuk <goncharuk.bro@gmail.com>
2023-05-13 21:13:36 +0300

    feat: add clear script

---
+++ utils/clearDeclarationDir.js
@@ -0,0 +1,19 @@
+const fs = require('fs');
+const util = require('util');
+
+const getPaths = require('./getPaths');
+
+const nodePaths = getPaths('./dist/node');
+const declarationPaths = getPaths('./dist/declaration');
+
+declarationPaths.forEach((path) => {
+  const normalizedPath = path
+    .replace(/^\.\/dist\/declaration/, './dist/node')
+    .replace(/d\.ts$/, 'cjs');
+
+  if (!nodePaths.includes(normalizedPath) && !/\/types\.d\.ts$/.test(path)) {
+    util
+      .promisify(fs.rm)(path)
+      .then(() => fs.rmdir(path.replace(/\/[^/]*$/gm, ''), () => {}));
+  }
+});

0bd66572d825b82d6b8efd0478801c767b657cb5
Nikolay Goncharuk <goncharuk.bro@gmail.com>
2023-05-14 23:56:38 +0300

    fix: update script

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -3,3 +3,16 @@ export { default as useConnectedInputRef } from './hooks/useConnectedInputRef';
 export { default as useDispatchCustomInputEvent } from './hooks/useDispatchCustomInputEvent';
 export { default as useInput } from './hooks/useInput';
 export { default as setInputAttributes } from './utils/setInputAttributes';
+
+export type {
+  CustomInputEvent,
+  CustomInputEventHandler,
+  InputType,
+  InputAttributes,
+  Init,
+  Tracking,
+  ExtendedHTMLInputElement,
+  PropsWithoutComponent,
+  PropsWithComponent,
+  InputComponent,
+} from './types';

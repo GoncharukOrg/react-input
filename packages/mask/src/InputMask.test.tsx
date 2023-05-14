5c22f149ff47a7b5e8441812b675111e7a79f5f0
Nikolay Goncharuk <goncharuk.bro@gmail.com>
2023-05-14 16:08:07 +0300

    feat: add tests

--- packages/mask/src/InputMask.test.tsx
+++ packages/mask/src/InputMask.test.tsx
@@ -4,31 +4,41 @@ import '@testing-library/jest-dom';
 import { render, screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 
-import { InputMask } from '.';
+import InputMask, { InputMaskProps } from './InputMask';
 
 const user = userEvent.setup({ delay: 15 });
 
-const init = () => {
-  render(<InputMask mask="+7 (___) ___-__-__" replacement="_" data-testid="input-mask" />);
+const init = (props: InputMaskProps = {}) => {
+  render(
+    <InputMask mask="+7 (___) ___-__-__" replacement="_" {...props} data-testid="input-mask" />
+  );
   return screen.getByTestId<HTMLInputElement>('input-mask');
 };
 
-const initWithDefaultType = async () => {
-  const input = init();
+const initWithDefaultType = async (props: InputMaskProps = {}) => {
+  const input = init(props);
   await user.type(input, '9123456789');
   return input;
 };
 
-test('Type', async () => {
+/**
+ * INSERT
+ */
+
+test('Insert', async () => {
   const input = await initWithDefaultType();
   expect(input).toHaveValue('+7 (912) 345-67-89');
 });
 
-test('Replace characters within the selected range', async () => {
+test('Insert with selection range', async () => {
   const input = await initWithDefaultType();
   await user.type(input, '0', { initialSelectionStart: 4, initialSelectionEnd: 8 });
   expect(input).toHaveValue('+7 (034) 567-89');
 });
+
+/**
+ * BACKSPACE
+ */
 
 test('Backspace after user character', async () => {
   const input = await initWithDefaultType();
@@ -41,6 +51,10 @@ test('Backspace after mask character', async () => {
   await user.type(input, '{Backspace}', { initialSelectionStart: 8, initialSelectionEnd: 8 });
   expect(input).toHaveValue('+7 (912) 345-67-89');
 });
+
+/**
+ * DELETE
+ */
 
 test('Delete before user character', async () => {
   const input = await initWithDefaultType();
---
+++ packages/number-format/src/InputNumberFormat.test.tsx
@@ -0,0 +1,179 @@
+import React from 'react';
+
+import '@testing-library/jest-dom';
+import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+
+import InputNumberFormat, { type InputNumberFormatProps } from './InputNumberFormat';
+
+const user = userEvent.setup({ delay: 15 });
+
+const init = (props: InputNumberFormatProps = {}) => {
+  render(<InputNumberFormat {...props} data-testid="input-number-format" />);
+  return screen.getByTestId<HTMLInputElement>('input-number-format');
+};
+
+const initWithDefaultType = async (props: InputNumberFormatProps = {}) => {
+  const input = init(props);
+  await user.type(input, '1234');
+  return input;
+};
+
+/**
+ * INSERT
+ */
+
+// default
+
+test('Insert', async () => {
+  const input = await initWithDefaultType();
+  expect(input).toHaveValue('1 234');
+});
+
+test('Insert without selection range', async () => {
+  const input = await initWithDefaultType();
+  await user.type(input, '9', { initialSelectionStart: 3, initialSelectionEnd: 3 });
+  expect(input).toHaveValue('12 934');
+});
+
+test('Insert less than selection range', async () => {
+  const input = await initWithDefaultType();
+  await user.type(input, '9', { initialSelectionStart: 2, initialSelectionEnd: 4 });
+  expect(input).toHaveValue('194');
+});
+
+test('Insert more than selection range', async () => {
+  const input = await initWithDefaultType();
+  await user.type(input, '6789', { initialSelectionStart: 2, initialSelectionEnd: 4 });
+  expect(input).toHaveValue('167 894');
+});
+
+// minimumIntegerDigits: 4
+
+test('Insert without selection range', async () => {
+  const input = await initWithDefaultType({ minimumIntegerDigits: 4 });
+  await user.type(input, '9', { initialSelectionStart: 3, initialSelectionEnd: 3 });
+  expect(input).toHaveValue('12 934');
+});
+
+test('Insert less than selection range', async () => {
+  const input = await initWithDefaultType({ minimumIntegerDigits: 4 });
+  await user.type(input, '9', { initialSelectionStart: 2, initialSelectionEnd: 4 });
+  expect(input).toHaveValue('0 194');
+});
+
+test('Insert more than selection range', async () => {
+  const input = await initWithDefaultType({ minimumIntegerDigits: 4 });
+  await user.type(input, '6789', { initialSelectionStart: 2, initialSelectionEnd: 4 });
+  expect(input).toHaveValue('167 894');
+});
+
+// minimumIntegerDigits: 6
+
+test('Insert without selection range', async () => {
+  const input = await initWithDefaultType({ minimumIntegerDigits: 6 });
+  await user.type(input, '9', { initialSelectionStart: 5, initialSelectionEnd: 5 });
+  expect(input).toHaveValue('012 934');
+});
+
+test('Insert less than selection range', async () => {
+  const input = await initWithDefaultType({ minimumIntegerDigits: 6 });
+  await user.type(input, '9', { initialSelectionStart: 4, initialSelectionEnd: 6 });
+  expect(input).toHaveValue('000 194');
+});
+
+test('Insert more than selection range', async () => {
+  const input = await initWithDefaultType({ minimumIntegerDigits: 6 });
+  await user.type(input, '6789', { initialSelectionStart: 4, initialSelectionEnd: 6 });
+  expect(input).toHaveValue('167 894');
+});
+
+/**
+ * BACKSPACE
+ */
+
+test('Backspace without selection range', async () => {
+  const input = await initWithDefaultType();
+  await user.type(input, '{Backspace}', { initialSelectionStart: 4, initialSelectionEnd: 4 });
+  expect(input).toHaveValue('124');
+});
+
+test('Backspace with selection range', async () => {
+  const input = await initWithDefaultType();
+  await user.type(input, '{Backspace}', { initialSelectionStart: 2, initialSelectionEnd: 4 });
+  expect(input).toHaveValue('14');
+});
+
+// minimumIntegerDigits: 4
+
+test('Backspace without selection range', async () => {
+  const input = await initWithDefaultType({ minimumIntegerDigits: 4 });
+  await user.type(input, '{Backspace}', { initialSelectionStart: 4, initialSelectionEnd: 4 });
+  expect(input).toHaveValue('0 124');
+});
+
+test('Backspace with selection range', async () => {
+  const input = await initWithDefaultType({ minimumIntegerDigits: 4 });
+  await user.type(input, '{Backspace}', { initialSelectionStart: 2, initialSelectionEnd: 4 });
+  expect(input).toHaveValue('0 014');
+});
+
+// minimumIntegerDigits: 6
+
+test('Backspace without selection range', async () => {
+  const input = await initWithDefaultType({ minimumIntegerDigits: 6 });
+  await user.type(input, '{Backspace}', { initialSelectionStart: 6, initialSelectionEnd: 6 });
+  expect(input).toHaveValue('000 124');
+});
+
+test('Backspace with selection range', async () => {
+  const input = await initWithDefaultType({ minimumIntegerDigits: 6 });
+  await user.type(input, '{Backspace}', { initialSelectionStart: 4, initialSelectionEnd: 6 });
+  expect(input).toHaveValue('000 014');
+});
+
+/**
+ * DELETE
+ */
+
+// default
+
+test('Delete without selection range', async () => {
+  const input = await initWithDefaultType();
+  await user.type(input, '{Delete}', { initialSelectionStart: 3, initialSelectionEnd: 3 });
+  expect(input).toHaveValue('124');
+});
+
+test('Delete with selection range', async () => {
+  const input = await initWithDefaultType();
+  await user.type(input, '{Delete}', { initialSelectionStart: 2, initialSelectionEnd: 4 });
+  expect(input).toHaveValue('14');
+});
+
+// minimumIntegerDigits: 4
+
+test('Delete without selection range', async () => {
+  const input = await initWithDefaultType({ minimumIntegerDigits: 4 });
+  await user.type(input, '{Delete}', { initialSelectionStart: 3, initialSelectionEnd: 3 });
+  expect(input).toHaveValue('0 124');
+});
+
+test('Delete with selection range', async () => {
+  const input = await initWithDefaultType({ minimumIntegerDigits: 4 });
+  await user.type(input, '{Delete}', { initialSelectionStart: 2, initialSelectionEnd: 4 });
+  expect(input).toHaveValue('0 014');
+});
+
+// minimumIntegerDigits: 6
+
+test('Delete without selection range', async () => {
+  const input = await initWithDefaultType({ minimumIntegerDigits: 6 });
+  await user.type(input, '{Delete}', { initialSelectionStart: 5, initialSelectionEnd: 5 });
+  expect(input).toHaveValue('000 124');
+});
+
+test('Delete with selection range', async () => {
+  const input = await initWithDefaultType({ minimumIntegerDigits: 6 });
+  await user.type(input, '{Delete}', { initialSelectionStart: 4, initialSelectionEnd: 6 });
+  expect(input).toHaveValue('000 014');
+});

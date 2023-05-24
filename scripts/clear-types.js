f9f6f3461b8248a805700a884eb5f6de131cc223
Nikolay Goncharuk <goncharuk.bro@gmail.com>
2023-05-24 23:07:03 +0300

    fix: restore build files

--- scripts/clear-types.js
+++ scripts/clear-types.js
@@ -4,12 +4,10 @@ const util = require('util');
 const getPaths = require('../utils/getPaths');
 
 const nodePaths = getPaths('./dist/node');
-const declarationPaths = getPaths('./dist/declaration');
+const typesPaths = getPaths('./dist/@types');
 
-declarationPaths.forEach((path) => {
-  const normalizedPath = path
-    .replace(/^\.\/dist\/declaration/, './dist/node')
-    .replace(/d\.ts$/, 'cjs');
+typesPaths.forEach((path) => {
+  const normalizedPath = path.replace(/^\.\/dist\/@types/, './dist/node').replace(/d\.ts$/, 'cjs');
 
   if (!nodePaths.includes(normalizedPath) && !/\/types\.d\.ts$/.test(path)) {
     util
---
+++ scripts/restore-dist.js
@@ -0,0 +1,13 @@
+const fs = require('fs');
+
+if (fs.existsSync('./dist/module')) {
+  throw new Error('The folder "module" already exists!');
+}
+
+fs.mkdirSync('./dist/module');
+
+fs.readdirSync('./dist').forEach((relativePath) => {
+  if (relativePath !== 'module' && relativePath !== 'node' && relativePath !== '@types') {
+    fs.renameSync(`./dist/${relativePath}`, `./dist/module/${relativePath}`);
+  }
+});

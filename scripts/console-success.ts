3acb3652fce91917123f5c7ced1be1e314c2aa5f
Nikolay Goncharuk <goncharuk.bro@gmail.com>
2024-03-30 10:00:51 +0300

    chore: :hammer: add "release" script

--- scripts/console-success.ts
+++ scripts/console-success.ts
@@ -1,35 +1,10 @@
-const style = {
-  apply: {
-    reset: '\x1b[0m',
-    bright: '\x1b[1m',
-    dim: '\x1b[2m',
-    underscore: '\x1b[4m',
-    blink: '\x1b[5m',
-    reverse: '\x1b[7m',
-    hidden: '\x1b[8m',
-  },
-  fg: {
-    black: '\x1b[30m',
-    red: '\x1b[31m',
-    green: '\x1b[32m',
-    yellow: '\x1b[33m',
-    blue: '\x1b[34m',
-    magenta: '\x1b[35m',
-    cyan: '\x1b[36m',
-    white: '\x1b[37m',
-  },
-  bg: {
-    black: '\x1b[40m',
-    red: '\x1b[41m',
-    green: '\x1b[42m',
-    yellow: '\x1b[43m',
-    blue: '\x1b[44m',
-    magenta: '\x1b[45m',
-    cyan: '\x1b[46m',
-    white: '\x1b[47m',
-  },
-};
+/* eslint-disable @typescript-eslint/no-unsafe-member-access */
+
+import style from '../utils/style';
 
 const packageName = `${process.env.npm_package_name}@${process.env.npm_package_version}`;
 
-console.log(`${style.fg.yellow}The package ${style.fg.blue}${packageName} ${style.fg.yellow}was successfully built!`);
+console.log(
+  `${style.fg.yellow}The package ${style.fg.blue}${packageName} ${style.fg.yellow}was successfully built!`,
+  style.reset,
+);
---
+++ scripts/release.ts
@@ -0,0 +1,53 @@
+import { execSync } from 'child_process';
+import fs from 'fs';
+import readline from 'readline';
+
+import corePackage from '../packages/core/package.json';
+import style from '../utils/style';
+
+const { npm_package_json, npm_package_name, npm_package_version } = process.env;
+
+if (!npm_package_json || !npm_package_name || !npm_package_version) {
+  throw new Error('');
+}
+
+const rl = readline.createInterface({ input: process.stdin, output: process.stdout });
+
+rl.question(`Do you want to update the ${npm_package_name} package? `, (answer) => {
+  if (answer.toLowerCase() !== 'y') {
+    rl.close();
+    process.exit();
+  }
+
+  if (npm_package_name !== '@react-input/core') {
+    fs.writeFileSync(
+      npm_package_json,
+      fs
+        .readFileSync(npm_package_json, 'utf8')
+        .replace(/"@react-input\/core": "\^\d\.\d\.\d"/, `"@react-input/core": "^${corePackage.version}"`),
+    );
+  }
+
+  const output = execSync(`npm version ${process.argv[2]}`, { encoding: 'utf8' });
+
+  const packageName = npm_package_name.replace(/^.+\/(.+)/, '$1');
+  const newPackageVersion = output.match(/\d+\.\d+\.\d+/)?.[0];
+
+  if (!newPackageVersion) {
+    throw new Error('');
+  }
+
+  const commit = `${packageName}/v${newPackageVersion}`;
+
+  execSync(
+    `git add ../../ && git commit -m "${commit}" && git tag ${commit} && git push && git push origin --tags && npm publish`,
+  );
+
+  console.log(
+    `${style.fg.yellow}The version of the package ${style.fg.blue}${npm_package_name} ${style.fg.yellow}has been updated!`,
+    style.reset,
+    `\n${npm_package_version} -> ${newPackageVersion}\n`,
+  );
+
+  rl.close();
+});
